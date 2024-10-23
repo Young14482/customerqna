3412d65df03f9ee78b5e5ff430eaa1bb7518bdab
young <young14482@gmail.com>
2024-10-23 12:03:28 +0900

    Feat 변수 및 메소드 이름 수정

--- src/main/java/kr/co/greenart/web/qna/QNA.java
+++ src/main/java/kr/co/greenart/web/qna/QNA.java
@@ -1,5 +1,7 @@
 package kr.co.greenart.web.qna;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,15 +11,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Qna {
-	private int article_id;
+public class QNA {
+	private Integer article_id;
 	private String title;
 	private String content;
 	private String username;
 	private String password;
-	private int views;
-	private String created_at;
-	private String updated_at;
-	private int is_secure;
-	private int is_deleted;
+	private Integer views;
+	private LocalDateTime created_at;
+	private LocalDateTime updated_at;
+	private Boolean secure;
+	private Boolean deleted;
 }
--- src/main/java/kr/co/greenart/web/qna/QnaMapper.java
+++ src/main/java/kr/co/greenart/web/qna/QnaMapper.java
@@ -7,30 +7,35 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+
 @Mapper
 public interface QnaMapper {
-	// 2번
+	// 글작성
 	@Insert(value = {"INSERT INTO customerqna (title, content, username, password)"
 			, " VALUES (#{title}, #{content}, #{username}, #{password})"})
-	void pooping(String title, String content, String username, String password);
+	void save(String title, String content, String username, String password);
 	
-	// 3번
+	// 게시글 목록 (article_id, title, username, is_secure)
 	@Select("SELECT article_id, title, username, is_secure FROM customerqna WHERE is_deleted = 0 AND is_secure = 0")
-	List<Qna> findAll();
+	void findAll();
 	
-	// 4번
+	// 게시글 조회 시 is_secure의 값이 false인 행만 조회
 	@Select("SELECT title, content, user FROM customerqna WHERE article_id = #{article_id} AND is_secure = 0")
-	List<Qna> findById(int article_id);
+	void findBySecureIsFalse(int article_id);
+
+	// 게시글 조회 (id로 검색, title, content, username)
+	@Select("SELECT title, content, user FROM customerqna WHERE article_id = #{article_id} AND is_secure = 0")
+	void findByPk(int article_id);
 	
-	// 5번
+	// 게시글의 비밀 여부 조회 (is_secure)
 	@Select("SELECT is_secure FROM customerqna WHERE article_id = #{article_id}")
-	Integer isSecure(int article_id);
+	void findSecureByPk(int article_id);
 	
-	// 6번
+	// views count 수정(1 증가)
 	@Update("UPDATE customerqna SET views = (views + 1) WHERE article_id = #{article_id}")
-	void viewContent(int article_id);
+	void updateCount(int article_id);
 	
-	// 7번
+	// 글 논리 삭제(pk 및 password일치) : is delete -> 1
 	@Update("UPDATE customerqna SET is_deleted = 1 WHERE article_id = #{article_id} AND password = #{password}")
-	void deleteQna(int article_id, String password);
+	void updateDelete(int article_id, String password);
 }
